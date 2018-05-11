719e452c7f03edbb1fe1d77b5d86743462305c02
everedifice <romz1212@gmail.com>
2018-05-11 15:13:39 +0900

    fix: script cwd

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+
 const pretty = require('pretty');
 const {resolve} = require('path');
 const {readFile, pathExistsSync, outputFile} = require('fs-extra');
 const [, , ...files] = process.argv;
 
-files.map(path => path.startsWith('/') ? path : resolve(__dirname, path))
+files.map(path => path.startsWith('/') ? path : resolve(process.cwd(), path))
     .filter(path => pathExistsSync(path))
     .map(path => readFile(path).then(src => outputFile(path, pretty(String(src)))));
